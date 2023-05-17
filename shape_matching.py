b9ac2bce8c1b7c6a9646e1effb89286df91f4d7e
Qubav <124883831+Qubav@users.noreply.github.com>
2023-05-17 20:21:17 +0200

    small udpate

--- shape_matching.py
+++ shape_matching.py
@@ -35,14 +35,19 @@ class ShapeMatching():
         
         # creating array with image in grayscale
         img_gray: np.ndarray = cv.cvtColor(self.bottle.img, cv.COLOR_BGR2GRAY)
+
         # adding blur to img_gray 
         img_gray = cv.GaussianBlur(img_gray, GAUS_BLUR_KERNEL, cv.BORDER_DEFAULT)
+
         # thresholding img with experimentally determined threshold value
         _, thresh = cv.threshold(img_gray, THRESHOLD_VALUE, 255, cv.THRESH_BINARY)
+
         # finding contours on thresholded image
         cnt, _ = cv.findContours(thresh, cv.RETR_LIST, cv.CHAIN_APPROX_NONE)
+
         # creating blank matrix that will be used to draw bottle shape - filled contour
         blank = np.zeros((img_gray.shape), dtype=np.uint8)
+        
         # determining which one of found contours should be filled - last one is whole photo contour, second from the end is bottle contour
         x = len(cnt) - 1
 
