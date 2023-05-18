743bd7f99612dd2645412f779e102d045cc44ed9
Qubav <124883831+Qubav@users.noreply.github.com>
2023-05-18 16:17:49 +0200

    detecting liquid level and drawing line on its level works

--- liquid_level_detection.py
+++ liquid_level_detection.py
@@ -8,6 +8,9 @@ GREEN_UPPER_HSV_VALUES = np.array([70, 255, 255])
 GREEN_LOWER_HSV_VALUES = np.array([35, 50, 50])
 
 RECTANGLE_WIDTH_PERCENTAGE = 15
+MORPH_OPENING_KERNEL = np.ones((19, 19), np.uint8)
+LINE_COLOR = (0, 0, 255)
+LINE_THICKNESS = 8
 
 class LiquidLevelDetection:
 
@@ -18,10 +21,15 @@ class LiquidLevelDetection:
         self.img_hsv_masked: np.ndarray = None
         self.liquid_color: str = None
         self.img_hsv_rectangle: np.ndarray = None
+        self.liquid_level: float = None
 
         self.get_hsv_masked_img()
-        self.get_liquid_color()
         self.get_hsv_rectangle_masked_img()
+        self.get_liquid_color()
+
+        if self.liquid_presence is True:
+            self.get_liquid_level()
+            
 
     def get_hsv_masked_img(self):
         """Method creates hsv image of bottle photo and adds mask to it, then assigns it as attribute."""
@@ -52,7 +60,6 @@ class LiquidLevelDetection:
         mask = cv.rectangle(blank, [new_left, top], [new_right, bottom], (255, 255, 255), -1)
 
         self.img_hsv_rectangle = cv.bitwise_and(self.img_hsv_masked, self.img_hsv_masked, mask = mask,)
-
 
     def get_liquid_color(self):
         
@@ -89,6 +96,26 @@ class LiquidLevelDetection:
                 self.liquid_presence = True
             
     def get_liquid_level(self):
+        
+        if self.liquid_color == "yellow":
+            one_color_rectangle_masked = cv.inRange(self.img_hsv_rectangle, YELLOW_LOWER_HSV_VALUES, YELLOW_UPPER_HSV_VALUES)
+        else:
+            one_color_rectangle_masked = cv.inRange(self.img_hsv_rectangle, GREEN_LOWER_HSV_VALUES, GREEN_UPPER_HSV_VALUES)
 
-        x = 2
+        cv.morphologyEx(one_color_rectangle_masked, cv.MORPH_OPEN, MORPH_OPENING_KERNEL)
 
+        lat_hist_y = np.sum(self.bottle.shape_img, 1)
+
+        top = np.argmax(lat_hist_y >= 2550)
+        bottom = len(lat_hist_y) - np.argmax(lat_hist_y[::-1] >= 2550) - 1
+
+        new_lat_hist_y = np.sum(one_color_rectangle_masked, 1)
+        liquid_level = np.argmax(new_lat_hist_y >= 2550)
+
+        left = next(i for i, val in enumerate(self.bottle.shape_img[liquid_level]) if val > 0)
+        right = self.bottle.shape_img.shape[1] - next(i for i, val in enumerate(reversed(self.bottle.shape_img[liquid_level])) if val > 0)
+
+        self.liquid_level = (liquid_level - bottom) / (top - bottom) * 100
+        self.bottle.liquid_level = self.liquid_level
+
+        self.bottle.img_with_bb_and_liquid_lvl = cv.line(self.bottle.img_w_bounding_boxes.copy(), [left, liquid_level], [right, liquid_level], LINE_COLOR, LINE_THICKNESS)
