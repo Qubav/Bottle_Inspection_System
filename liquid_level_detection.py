8a34da1f9c5f34b5b4f9fe1c9ceb060b2ee0d36d
Qubav <124883831+Qubav@users.noreply.github.com>
2023-05-18 17:14:25 +0200

    adding docstrings and comments

--- liquid_level_detection.py
+++ liquid_level_detection.py
@@ -16,6 +16,7 @@ class LiquidLevelDetection:
 
     def __init__(self, bottle: BottlePhoto) -> None:
         
+        # defining variables
         self.bottle = bottle
         self.liquid_presence: bool = None
         self.img_hsv_masked: np.ndarray = None
@@ -23,14 +24,13 @@ class LiquidLevelDetection:
         self.img_hsv_rectangle: np.ndarray = None
         self.liquid_level: float = None
 
+        # executing methods
         self.get_hsv_masked_img()
         self.get_hsv_rectangle_masked_img()
         self.get_liquid_color()
-
         if self.liquid_presence is True:
             self.get_liquid_level()
             
-
     def get_hsv_masked_img(self):
         """Method creates hsv image of bottle photo and adds mask to it, then assigns it as attribute."""
 
@@ -38,7 +38,9 @@ class LiquidLevelDetection:
         self.img_hsv_masked = cv.bitwise_and(img_hsv, img_hsv, mask = self.bottle.shape_img)
     
     def get_hsv_rectangle_masked_img(self):
+        """Method creates hsv image with mask that only leaves rectangle with center of bottle."""
 
+        # creating blank
         blank = np.zeros((self.bottle.shape_img.shape), dtype=np.uint8)
 
         # getting lateral histogram to determine the object's location
@@ -51,21 +53,26 @@ class LiquidLevelDetection:
         top = np.argmax(lat_hist_y >= 2550)
         bottom = len(lat_hist_y) - np.argmax(lat_hist_y[::-1] >= 2550) - 1
 
+        # calculating coordinates
         bottle_width = right - left
         rectangle_width = int(bottle_width * RECTANGLE_WIDTH_PERCENTAGE /100)
-
         new_left = left + bottle_width // 2 - rectangle_width // 2
         new_right = right - bottle_width // 2 + rectangle_width // 2
 
+        # creating mask
         mask = cv.rectangle(blank, [new_left, top], [new_right, bottom], (255, 255, 255), -1)
 
+        # creating masked image and assigning it as attribute
         self.img_hsv_rectangle = cv.bitwise_and(self.img_hsv_masked, self.img_hsv_masked, mask = mask,)
 
     def get_liquid_color(self):
+        """Method check if there is liquid in bottle and if there is liquid checks its color."""
         
+        # creating images with shapes of one color pixels
         yellow_color_pixels_img = cv.inRange(self.img_hsv_masked, YELLOW_LOWER_HSV_VALUES, YELLOW_UPPER_HSV_VALUES)
         green_color_pixels_img = cv.inRange(self.img_hsv_masked, GREEN_LOWER_HSV_VALUES, GREEN_UPPER_HSV_VALUES)
 
+        # calculating number of pixels of each color
         shape_pixels_count = cv.countNonZero(self.bottle.shape_img)
         yellow_pixels_count = cv.countNonZero(yellow_color_pixels_img)
         green_pixels_count = cv.countNonZero(green_color_pixels_img)
@@ -96,26 +103,35 @@ class LiquidLevelDetection:
                 self.liquid_presence = True
             
     def get_liquid_level(self):
+        """Method checks liquid level and defines what percentage of bottle is filled and draws line at liquid levels height."""
         
+        # creating liquid shape image for correct colored pixels
         if self.liquid_color == "yellow":
             one_color_rectangle_masked = cv.inRange(self.img_hsv_rectangle, YELLOW_LOWER_HSV_VALUES, YELLOW_UPPER_HSV_VALUES)
         else:
             one_color_rectangle_masked = cv.inRange(self.img_hsv_rectangle, GREEN_LOWER_HSV_VALUES, GREEN_UPPER_HSV_VALUES)
 
+        # performing morphology opening operation
         cv.morphologyEx(one_color_rectangle_masked, cv.MORPH_OPEN, MORPH_OPENING_KERNEL)
 
+        # creating lateral histogram to get bottle position
         lat_hist_y = np.sum(self.bottle.shape_img, 1)
 
+        # getting coordinates
         top = np.argmax(lat_hist_y >= 2550)
         bottom = len(lat_hist_y) - np.argmax(lat_hist_y[::-1] >= 2550) - 1
 
+        # lateral histogram and coordinates for liquid
         new_lat_hist_y = np.sum(one_color_rectangle_masked, 1)
         liquid_level = np.argmax(new_lat_hist_y >= 2550)
 
+        # calculating bottle left and right coordinates at liquid level
         left = next(i for i, val in enumerate(self.bottle.shape_img[liquid_level]) if val > 0)
         right = self.bottle.shape_img.shape[1] - next(i for i, val in enumerate(reversed(self.bottle.shape_img[liquid_level])) if val > 0)
 
-        self.liquid_level = (liquid_level - bottom) / (top - bottom) * 100
+        # assigning variable to attributes
+        self.liquid_level = np.round((liquid_level - bottom) / (top - bottom) * 100, 2)
         self.bottle.liquid_level = self.liquid_level
 
+        # creating image with liquid level highlighted and assigning it as attribute
         self.bottle.img_with_bb_and_liquid_lvl = cv.line(self.bottle.img_w_bounding_boxes.copy(), [left, liquid_level], [right, liquid_level], LINE_COLOR, LINE_THICKNESS)
